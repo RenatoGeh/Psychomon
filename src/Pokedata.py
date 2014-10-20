bea40f5918edb9b2f6746a41bd3f8f4c673ba63f
Renato Lui Geh <renatogeh@gmail.com>
2014-10-20 18:27:03 -0200

    Pokedex.

--- src/Pokedata.py
+++ src/Pokedata.py
@@ -1,30 +1,31 @@
-POKE_TYPES = ("Normal", "Fighting", "Flying", "Poison", "Ground", "Rock", "Bird", "Bug", "Ghost", "Fire",
-	"Water", "Grass", "Electric", "Psychic", "Ice", "Dragon", "Blank")
+POKE_TYPES = ("Normal", "Fighting", "Flying", "Poison", "Ground", "Rock", "Bird", "Bug", "Ghost", "Fire", "Water", "Grass", "Electric", "Psychic", "Ice", "Dragon", "Blank")
+
+TYPE_BLANK = len(POKE_TYPES)-1
 
 class Attribute:
-	"Attribute class constructor."
-	def __init__(self, hp, atk, df, spd, spc):
-		self.hp = hp
-		self.atk = atk
-		self.df = df		# def is already a keyword.
-		self.spd = spd
-		self.spc = spc
-	
-	"Creates a cloned instance of same object."
-	def __copy__(self):
-		return Attribute(self.hp, self.atk, self.df, self.spd, self.spc)
+    "Attribute class constructor."
+    def __init__(self, hp, atk, df, spd, spc):
+        self.hp = hp
+        self.atk = atk
+        self.df = df        # def is already a keyword.
+        self.spd = spd
+        self.spc = spc
+    
+    "Creates a cloned instance of same object."
+    def __copy__(self):
+        return Attribute(self.hp, self.atk, self.df, self.spd, self.spc)
 
 class Attack:
-	"Attack class constructor."
-	def __init__(self, name, typ, acu, pwr, pp):
-		self.name = name
-		self.typ = typ
-		self.acu = acu
-		self.pwr = pwr
-		self.ppc = pp        #current pp of attack
-        self.ppi = pp        #initial pp of attack
-	"Produces the attack on target opponent."
-	def attack(self, opponent):
-		# Do attack stuff.
-		pass	 
-		
+    "Attack class constructor."
+    def __init__(self, name, typ, acu, pwr, pp):
+        self.name = name
+        self.typ = typ
+        self.acu = acu
+        self.pwr = pwr
+        self.base_pp = self.curr_pp = pp
+    
+    "Produces the attack on target opponent."
+    def attack(self, me, opponent):
+        # Do attack stuff.
+        pass
+        
--- src/Pokemon.py
+++ src/Pokemon.py
@@ -2,15 +2,15 @@ import Pokedata
 import copy
 
 class Pokemon:
-	"Pokemon class constructor."
-	def __init__(self, atts, atks, type1, type2="Blank"):
-		self.atts = atts
-		self.current_atts = copy.copy(atts)	# Current attribute takes into account ATT altering attacks.
-		self.atks = atks
-		self.types = (type1, type2)
-	
-	"This pokemon attacks target opponent pokemon with given attack."
-	def attack(self, opponent, atk_index):
-		self.atks[atk_index].attack(opponent)
+    "Pokemon class constructor."
+    def __init__(self, atts, atks, type1, type2=Pokedata.TYPE_BLANK):
+        self.atts = atts
+        self.current_atts = copy.copy(atts) # Current attribute takes into account ATT altering attacks.
+        self.atks = atks
+        self.types = (type1, type2)
+    
+    "This pokemon attacks target opponent pokemon with given attack."
+    def attack(self, opponent, atk_index):
+        self.atks[atk_index].attack(self, opponent)
 
-		
+        
